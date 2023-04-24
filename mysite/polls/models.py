b174ab1bfa3d703f350fa65828a0d7d5333e5146
pjw74 <70009161+pjw74@users.noreply.github.com>
2023-04-25 00:03:44 +0900

    0425

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,10 +13,20 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("data published")
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    # score = models.FloatField(default=0)
+    # is_something_wrong = models.BooleanField(default=False)
+    # json_field = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f"제목: {self.question_text}, 날짜: {self.pub_date}"
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
