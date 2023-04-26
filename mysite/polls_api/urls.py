91b5bbab16accedd4453960cebd000d0dc34d67e
pjw74 <70009161+pjw74@users.noreply.github.com>
2023-04-26 18:27:40 +0900

    0426

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('question/', question_list, name='question-list')
+    path('question/', question_list, name='question-list'),
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
 # Create your views here.
+from polls.models import Question
+from polls_api.serializers import QuestionSerializer
+from rest_framework.response import Response
+
+
+@api_view()
+def question_list(request):
+    question = Question.objects.all()
+    serializer = QuestionSerializer(question, many=True)
+    return Response(serializer.data)
+
+
+
